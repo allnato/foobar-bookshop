d00b8d61b49fc9e9933a4268ff87467142be6da8
Allendale <natoallendale@gmail.com>
2017-01-18 15:46:18 +0800

    Update foobar DB model
    + Added address table
    + Tweaked product list and cart table

---
+++ foobarDB/foobarDB-ver02.sql
@@ -0,0 +1,377 @@
+CREATE DATABASE  IF NOT EXISTS `foobar_booksop` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `foobar_booksop`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: foobar_booksop
+-- ------------------------------------------------------
+-- Server version	5.7.10-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cart` (
+  `cartID` int(10) unsigned NOT NULL,
+  `customerID` int(10) unsigned NOT NULL,
+  `totalQuantity` int(10) unsigned NOT NULL,
+  `totalAmount` double unsigned NOT NULL,
+  `status` enum('open','closed') NOT NULL,
+  PRIMARY KEY (`cartID`),
+  UNIQUE KEY `cartID_UNIQUE` (`cartID`),
+  UNIQUE KEY `customerID_UNIQUE` (`customerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `credit_cards`
+--
+
+DROP TABLE IF EXISTS `credit_cards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `credit_cards` (
+  `creditcardID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `cardNum` varchar(10) NOT NULL,
+  `type` enum('visa','master card') NOT NULL,
+  `expDate` date NOT NULL,
+  PRIMARY KEY (`creditcardID`),
+  UNIQUE KEY `creditcardID_UNIQUE` (`creditcardID`),
+  UNIQUE KEY `cardNum_UNIQUE` (`cardNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `credit_cards`
+--
+
+LOCK TABLES `credit_cards` WRITE;
+/*!40000 ALTER TABLE `credit_cards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `credit_cards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer_address`
+--
+
+DROP TABLE IF EXISTS `customer_address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer_address` (
+  `customerID` int(11) NOT NULL,
+  `addressType` enum('billing','delivery') NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `zipcode` varchar(45) DEFAULT NULL,
+  `region` varchar(45) NOT NULL,
+  `country` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer_address`
+--
+
+LOCK TABLES `customer_address` WRITE;
+/*!40000 ALTER TABLE `customer_address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer_address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer_cards`
+--
+
+DROP TABLE IF EXISTS `customer_cards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer_cards` (
+  `customerID` int(10) unsigned NOT NULL,
+  `creditcardID` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer_cards`
+--
+
+LOCK TABLES `customer_cards` WRITE;
+/*!40000 ALTER TABLE `customer_cards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer_cards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `customerID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `dateRegistered` datetime NOT NULL,
+  PRIMARY KEY (`customerID`),
+  UNIQUE KEY `customerID_UNIQUE` (`customerID`),
+  UNIQUE KEY `userID_UNIQUE` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employees` (
+  `employeeID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userID` int(10) unsigned NOT NULL,
+  `dateHired` date NOT NULL,
+  `employeeType` enum('Product Manager','Administrator','Account Manager') NOT NULL,
+  PRIMARY KEY (`employeeID`),
+  UNIQUE KEY `employeeID_UNIQUE` (`employeeID`),
+  UNIQUE KEY `userID_UNIQUE` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `productID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` double unsigned NOT NULL,
+  `avgRating` double unsigned DEFAULT NULL,
+  PRIMARY KEY (`productID`),
+  UNIQUE KEY `productID_UNIQUE` (`productID`),
+  UNIQUE KEY `avgRating_UNIQUE` (`avgRating`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_list`
+--
+
+DROP TABLE IF EXISTS `product_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_list` (
+  `cartID` int(10) unsigned NOT NULL,
+  `productID` int(10) unsigned NOT NULL,
+  `productQuantity` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cartID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_list`
+--
+
+LOCK TABLES `product_list` WRITE;
+/*!40000 ALTER TABLE `product_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_type`
+--
+
+DROP TABLE IF EXISTS `product_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_type` (
+  `typeID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `managerID` int(10) unsigned NOT NULL,
+  `type` varchar(25) NOT NULL,
+  PRIMARY KEY (`typeID`),
+  UNIQUE KEY `typeID_UNIQUE` (`typeID`),
+  UNIQUE KEY `type_UNIQUE` (`type`),
+  UNIQUE KEY `managerID_UNIQUE` (`managerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_type`
+--
+
+LOCK TABLES `product_type` WRITE;
+/*!40000 ALTER TABLE `product_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `review` (
+  `reviewID` int(11) NOT NULL,
+  `customerID` int(10) unsigned NOT NULL,
+  `rating` int(10) unsigned NOT NULL,
+  `message` varchar(45) NOT NULL,
+  `dateReviewed` datetime NOT NULL,
+  PRIMARY KEY (`reviewID`),
+  UNIQUE KEY `reviewID_UNIQUE` (`reviewID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review`
+--
+
+LOCK TABLES `review` WRITE;
+/*!40000 ALTER TABLE `review` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `transactionID` int(10) unsigned NOT NULL,
+  `cartID` int(10) unsigned NOT NULL,
+  `datePurchased` datetime NOT NULL,
+  `totalAmount` double unsigned NOT NULL,
+  PRIMARY KEY (`transactionID`),
+  UNIQUE KEY `transactionID_UNIQUE` (`transactionID`),
+  UNIQUE KEY `cartID_UNIQUE` (`cartID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_events`
+--
+
+DROP TABLE IF EXISTS `user_events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_events` (
+  `userID` int(11) unsigned NOT NULL,
+  `alertType` varchar(15) NOT NULL,
+  `description` varchar(128) NOT NULL,
+  `dateTriggered` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_events`
+--
+
+LOCK TABLES `user_events` WRITE;
+/*!40000 ALTER TABLE `user_events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `userID` int(10) unsigned NOT NULL,
+  `firstname` varchar(35) NOT NULL,
+  `lastname` varchar(35) NOT NULL,
+  `middleinitial` varchar(5) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `status` enum('active','banned') NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `userID_UNIQUE` (`userID`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-18 15:45:31
