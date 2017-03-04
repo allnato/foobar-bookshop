26bf152b80f9fba6b61e4b74304fee2031d86ffa
Allendale Nato <natoallendale@gmail.com>
2017-03-04 20:53:17 +0800

    Add action bar functionality

--- pages/js/catalog.js
+++ pages/js/catalog.js
@@ -1,3 +1,71 @@
+var qsRegex;
+var $productItems = $('.catalog-items').isotope({
+  itemSelector: '.item-card',
+  getSortData: {
+    name: '.item-name',
+    price: '.item-price parseFloat',
+    reviews: '.item-review parseInt',
+    date: '.item-update'
+  },
+  filter: function() {
+    return qsRegex ? $(this).text().match( qsRegex ) : true;
+  }
+})
+
+// use value of search field to filter
+var $quicksearch = $('#search-filter').keyup( debounce( function() {
+  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+  $productItems.isotope();
+}, 200 ) );
+
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  return function debounced() {
+    if ( timeout ) {
+      clearTimeout( timeout );
+    }
+    function delayed() {
+      fn();
+      timeout = null;
+    }
+    timeout = setTimeout( delayed, threshold || 100 );
+  }
+}
+
+var sort_direction = 'ascending';
+var sort_type = 'name';
+
+$('.sort-direction-btn').click(function(event) {
+  if (sort_direction === 'ascending'){
+    sort_direction = 'descending';
+    $productItems.isotope({
+      sortBy : sort_type,
+      sortAscending: false
+    });
+    $('.sort-direction-text').text('Descending');
+    $('.sort-direction-logo').removeClass('fa-long-arrow-up');
+    $('.sort-direction-logo').addClass('fa-long-arrow-down');
+  }
+  else {
+    sort_direction = 'ascending';
+    $productItems.isotope({
+      sortBy : sort_type,
+      sortAscending: true
+    });
+    $('.sort-direction-text').text('Ascending');
+    $('.sort-direction-logo').removeClass('fa-long-arrow-down');
+    $('.sort-direction-logo').addClass('fa-long-arrow-up');
+  }
+});
+
+$('.sort-type').click(function(event) {
+  sort_type = $(this).text().toLowerCase();
+  $('.sort-type-text').text(sort_type);
+  $productItems.isotope({sortBy : sort_type});
+});
+
 $('.sidebar-nav').stick_in_parent({
   inner_scrolling: false,
-  bottoming: false});
+  bottoming: false
+});
