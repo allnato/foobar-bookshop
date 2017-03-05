65e43019d76d7dd3c28d5a102954c99fd16f242c
felt20 <joshua_patrick_ramos@dlsu.edu.ph>
2017-03-05 16:30:42 +0800

    Add AES encryption-decryption

--- FooBarBookshop/src/java/edu/secprog/security/AES.java
+++ FooBarBookshop/src/java/edu/secprog/security/AES.java
@@ -5,30 +5,14 @@
  */
 package edu.secprog.security;
 
-import edu.secprog.dto.Customer;
-
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.crypto.BadPaddingException;
+import java.util.Arrays;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
@@ -36,101 +20,115 @@ import javax.crypto.spec.IvParameterSpec;
  */
 public class AES {
     
-    private static IvParameterSpec ivParameterSpec;
-    
-    // here, it is expected that there's a password entry already
-    public static void encryptString(String key, Customer nc) {
-        byte[] encrypted = null;
-        ResultSet rs = null;
-        
-        KeyGenerator KeyGen = null;
-        try {
-            KeyGen = KeyGenerator.getInstance("AES");
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        KeyGen.init(256);
-        
-        SecretKey SecKey = KeyGen.generateKey();
-        Cipher AesCipher = null;
-        try {
-            AesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException ex) {
-            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        byte[] keyByte = key.getBytes();
-        byte[] initVec = new byte[16];
-        
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(initVec);
-        ivParameterSpec = new IvParameterSpec(initVec);
-        
-        try {
-            AesCipher.init(Cipher.ENCRYPT_MODE, SecKey, ivParameterSpec);
-        } catch (InvalidKeyException | InvalidAlgorithmParameterException ex) {
-            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            encrypted = AesCipher.doFinal(keyByte);
-        } catch (IllegalBlockSizeException | BadPaddingException ex) {
-            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        if(encrypted != null && encrypted.length != 0) {
-        
-            long insertID = -1;
-            PreparedStatement pstmt = null;
-            Connection connection = null;
-            
-            /* Step 1: Add the data to the user table, regardless if employee or customer
-             */
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/foobar_booksop", "test", "1234");
-                pstmt = connection.prepareStatement("INSERT INTO passwords(encrypted, vector"
-                        + ",timestamp) values(?,?,?)", Statement.RETURN_GENERATED_KEYS);
-                pstmt.setBytes(1, encrypted);
-                pstmt.setBytes(2, initVec);
-                pstmt.setTimestamp(3, Audit.getCurrentTimeStamp());
-                int affectedRows = pstmt.executeUpdate();
+    public static byte[] setVector() { // used before encryption in registration / changing of password
+    	  byte[] digestKey = new byte[16];
+    	  MessageDigest sha = null;
+    	  
+    	  SecureRandom random = new SecureRandom();
+          random.nextBytes(digestKey);
+    	  
+    	  try {
+              sha = MessageDigest.getInstance("SHA-256");
+              digestKey = sha.digest(digestKey);
+              digestKey = Arrays.copyOf(digestKey, 16);
+              
+              return digestKey;
+          } 
+          catch (NoSuchAlgorithmException e) {
+              e.printStackTrace();
+          }
+    	  
+    	  return null;
+      }
+      
+      public static byte[] encryptString(String key, byte[] initVector) {
+    	  try {
+    		  byte[] encrypted;
+    		  
+    		  AESEncrypter encrypter = new AESEncrypter(initVector);
+    		  encrypted = encrypter.encrypt(key);
+    		  
+    		  return encrypted;
+    		  
+    	  } catch (Exception e) {
+              e.printStackTrace();
+          }
+    	  
+    	  return null;
+      }
+      
+      public static String decryptString(byte[] key, byte[] initVector) {
+    	  try {
+    		  String decrypted;
+    		  
+    		  AESEncrypter encrypter = new AESEncrypter(initVector);
+    		  decrypted = encrypter.decrypt(key);
+    		  
+    		  return decrypted;
+    		  
+    	  } catch (Exception e) {
+              e.printStackTrace();
+          }
+    	  
+    	  return null;
+      }
+      
+    private static class AESEncrypter {
+        Cipher ecipher;
+        Cipher dcipher;
 
-                if (affectedRows == 0) {
-                    throw new SQLException("Register failed! No rows affected");
-                }
-                // Check if registering is successful. Return ID generated
-                try {
-                    rs = pstmt.getGeneratedKeys();
-                    if(rs.next()) {
-                        insertID = rs.getLong(1);
-                    }
-                    else {
-                        throw new SQLException("Register failed! No rows affected");
-                    }
-                } catch(SQLException e) {
-                    System.out.println("Register failed! No rows affected");
-                    }
-                // End CHeck if registering is successful
-            } catch(ClassNotFoundException | SQLException e) {
-                //put future audit codes here in the future
-                }
-            finally {
-                try {
-                    pstmt.close();
-                } catch (SQLException ex) {
-                    Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                try {
-                    connection.close();
-                } catch (SQLException ex) {
-                    Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
-                }
+        AESEncrypter(byte[] digestKey) {
+            try {
+            	SecretKeySpec key = new SecretKeySpec(digestKey, "AES");
+            	
+                ecipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                dcipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                
+                byte[] eiv = new byte[ecipher.getBlockSize()];
+                byte[] div = new byte[dcipher.getBlockSize()];
+                
+                IvParameterSpec eivParams = new IvParameterSpec(eiv);
+                IvParameterSpec divParams = new IvParameterSpec(div);
+                
+                ecipher.init(Cipher.ENCRYPT_MODE, key, eivParams);
+                dcipher.init(Cipher.DECRYPT_MODE, key, divParams);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
-    }
-    
-    public static String decryptString(byte[] key) {
-        return null;
+
+        public byte[] encrypt(String str) {
+            try {
+                // Encode the string into bytes using utf-8
+                byte[] utf8 = str.getBytes("UTF8");
+
+                // Encrypt
+                byte[] encrypted = null;
+                try {
+                    encrypted = ecipher.doFinal(utf8);
+                } catch (IllegalBlockSizeException ex) {
+                    ex.printStackTrace(); // soon log this on Audit
+                }
+
+                return encrypted;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        public String decrypt(byte[] key) {
+            try {
+                // Decrypt
+                byte[] utf8 = dcipher.doFinal(key);
+
+                // Decode using utf-8
+                return new String(utf8, "UTF8");
+            } catch (Exception ex) {
+            	ex.printStackTrace();
+            }
+            return null;
+        }
     }
 }
