e5314814160933bc2977eef3637bd74f3d4feba1
felt20 <joshua_patrick_ramos@dlsu.edu.ph>
2017-03-05 11:35:39 +0800

    Add audit and SecurityService

--- FooBarBookshop/src/java/edu/secprog/security/AES.java
+++ FooBarBookshop/src/java/edu/secprog/security/AES.java
@@ -5,10 +5,21 @@
  */
 package edu.secprog.security;
 
+import edu.secprog.dto.Customer;
+
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
@@ -27,8 +38,10 @@ public class AES {
     
     private static IvParameterSpec ivParameterSpec;
     
-    public static byte[] encryptString(String key) {
+    // here, it is expected that there's a password entry already
+    public static void encryptString(String key, Customer nc) {
         byte[] encrypted = null;
+        ResultSet rs = null;
         
         KeyGenerator KeyGen = null;
         try {
@@ -64,7 +77,57 @@ public class AES {
             Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        return encrypted; // this may be null -- ensure that the calling function is enclosed with try-catch
+        if(encrypted != null && encrypted.length != 0) {
+        
+            long insertID = -1;
+            PreparedStatement pstmt = null;
+            Connection connection = null;
+            
+            /* Step 1: Add the data to the user table, regardless if employee or customer
+             */
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/foobar_booksop", "test", "1234");
+                pstmt = connection.prepareStatement("INSERT INTO passwords(encrypted, vector"
+                        + ",timestamp) values(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+                pstmt.setBytes(1, encrypted);
+                pstmt.setBytes(2, initVec);
+                pstmt.setTimestamp(3, Audit.getCurrentTimeStamp());
+                int affectedRows = pstmt.executeUpdate();
+
+                if (affectedRows == 0) {
+                    throw new SQLException("Register failed! No rows affected");
+                }
+                // Check if registering is successful. Return ID generated
+                try {
+                    rs = pstmt.getGeneratedKeys();
+                    if(rs.next()) {
+                        insertID = rs.getLong(1);
+                    }
+                    else {
+                        throw new SQLException("Register failed! No rows affected");
+                    }
+                } catch(SQLException e) {
+                    System.out.println("Register failed! No rows affected");
+                    }
+                // End CHeck if registering is successful
+            } catch(ClassNotFoundException | SQLException e) {
+                //put future audit codes here in the future
+                }
+            finally {
+                try {
+                    pstmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
     
     public static String decryptString(byte[] key) {
---
+++ FooBarBookshop/src/java/edu/secprog/services/SecurityService.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.secprog.services;
+
+import edu.secprog.security.AES;
+import edu.secprog.security.Audit;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author CoRX
+ */
+public class SecurityService {
+    
+    // main model for the security packages
+    
+    public void addPassword(String hashed, byte[] encrypted, byte[] vector, int userID) {
+        long insertID = -1;
+        PreparedStatement pstmt = null;
+        Connection connection = null;
+        ResultSet rs = null;
+
+        /* Step 1: Add the data to the user table, regardless if employee or customer
+         */
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/foobar_booksop", "test", "1234");
+            pstmt = connection.prepareStatement("INSERT INTO passwords(encrypted, vector"
+                    + ",timestamp) values(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+            pstmt.setBytes(1, encrypted);
+            pstmt.setBytes(2, vector);
+            pstmt.setTimestamp(3, Audit.getCurrentTimeStamp());
+            int affectedRows = pstmt.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Register failed! No rows affected");
+            }
+            // Check if registering is successful. Return ID generated
+            try {
+                rs = pstmt.getGeneratedKeys();
+                if(rs.next()) {
+                    insertID = rs.getLong(1);
+                }
+                else {
+                    throw new SQLException("Register failed! No rows affected");
+                }
+            } catch(SQLException e) {
+                System.out.println("Register failed! No rows affected");
+                }
+            // End CHeck if registering is successful
+        } catch(ClassNotFoundException | SQLException e) {
+            //put future audit codes here in the future
+            }
+        finally {
+            try {
+                pstmt.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
