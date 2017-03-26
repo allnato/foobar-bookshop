9f83f03d17fa4f5ec6bc38e0d25272219c4475ea
felt20 <joshua_patrick_ramos@dlsu.edu.ph>
2017-03-26 13:16:53 +0800

    Add error handling on registration

--- FooBarBookshop/src/java/edu/secprog/servlets/RegisterServlet.java
+++ FooBarBookshop/src/java/edu/secprog/servlets/RegisterServlet.java
@@ -78,75 +78,75 @@ public class RegisterServlet extends HttpServlet {
         Password ps = new Password();
         
         // Set user information
-        nc.setFirstname(request.getParameter("firstName"));
-        nc.setMiddleinitial(request.getParameter("middleInitial"));
-        nc.setLastname(request.getParameter("lastName"));
-        nc.setEmail(BCrypt.hashpw(request.getParameter("email"), BCrypt.gensalt(10)));
-        Date birthDate = null;
-        SimpleDateFormat sdf0 =
-            new SimpleDateFormat("yyyy-MM-dd");
         try {
-            birthDate = sdf0.parse(request.getParameter("birthDate"));
-        } catch (ParseException ex) {
-            Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        java.sql.Date sqlBirthDate = new java.sql.Date(birthDate.getTime());
- 
-        nc.setBirthdate(sqlBirthDate.toString());
-        nc.setUsername(request.getParameter("username"));
-        nc.setPassword(BCrypt.hashpw(request.getParameter("password"), BCrypt.gensalt(10)));
-        nc.setStatus("active");
-        Date dt = new Date();
-        SimpleDateFormat sdf =
-                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String dateRegistered = sdf.format(dt);
-        nc.setDateRegistered(dateRegistered);
-        // Set user address on a different object and table
-        // Billing Address
-        System.out.println(request.getParameter("b_address"));
-        System.out.println(request.getParameter("b_city"));
+            nc.setFirstname(request.getParameter("firstName"));
+            nc.setMiddleinitial(request.getParameter("middleInitial"));
+            nc.setLastname(request.getParameter("lastName"));
+            nc.setEmail(BCrypt.hashpw(request.getParameter("email"), BCrypt.gensalt(10)));
+            Date birthDate = null;
+            SimpleDateFormat sdf0 =
+                new SimpleDateFormat("yyyy-MM-dd");
+            try {
+                birthDate = sdf0.parse(request.getParameter("birthDate"));
+            } catch (ParseException ex) {
+                Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            java.sql.Date sqlBirthDate = new java.sql.Date(birthDate.getTime());
+
+            nc.setBirthdate(sqlBirthDate.toString());
+            nc.setUsername(request.getParameter("username"));
+            nc.setPassword(BCrypt.hashpw(request.getParameter("password"), BCrypt.gensalt(10)));
+            nc.setStatus("active");
+            Date dt = new Date();
+            SimpleDateFormat sdf =
+                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String dateRegistered = sdf.format(dt);
+            nc.setDateRegistered(dateRegistered);
+            // Set user address on a different object and table
+            // Billing Address
+            System.out.println(request.getParameter("b_address"));
+            System.out.println(request.getParameter("b_city"));
+
+            bca.setAddress(request.getParameter("b_address"));
+            bca.setCity(request.getParameter("b_city"));
+            bca.setZipcode(request.getParameter("b_zipcode"));
+            bca.setRegion(request.getParameter("b_region"));
+            bca.setCountry(request.getParameter("b_country"));
+            bca.setAddressType("billing");
+            // Delivery Debugging Purposes
+            System.out.println("Delivery Address");
+            System.out.println(bca.getAddress());
+            System.out.println(bca.getCity());
+            System.out.println(bca.getZipcode());
+            System.out.println(bca.getRegion());
+            System.out.println(bca.getCountry());
+            System.out.println(bca.getAddressType());
+            System.out.println(bca.getAddressType());
+            // Shipping Address
+            dca.setAddress(request.getParameter("d_address"));
+            dca.setCity(request.getParameter("d_city"));
+            dca.setZipcode(request.getParameter("d_zipcode"));
+            dca.setRegion(request.getParameter("d_region"));
+            dca.setCountry(request.getParameter("d_country"));
+            dca.setAddressType("delivery");
+            // Delivery Debugging Purposes
+            System.out.println("Delivery Address");
+            System.out.println(dca.getAddress());
+            System.out.println(dca.getCity());
+            System.out.println(dca.getZipcode());
+            System.out.println(dca.getRegion());
+            System.out.println(dca.getCountry());
+            System.out.println(dca.getAddressType());
+
+            // Set user credit card info
+            cc.setName(request.getParameter("cardName"));
+            cc.setCardNum(BCrypt.hashpw(request.getParameter("cardNum"), BCrypt.gensalt(10)));
+            cc.setType(request.getParameter("cardType"));
+            cc.setExpDate(request.getParameter("cardExp"));
+            System.out.println("YUNG CARD TYPE: " + request.getParameter("cardType"));
         
-        bca.setAddress(request.getParameter("b_address"));
-        bca.setCity(request.getParameter("b_city"));
-        bca.setZipcode(request.getParameter("b_zipcode"));
-        bca.setRegion(request.getParameter("b_region"));
-        bca.setCountry(request.getParameter("b_country"));
-        bca.setAddressType("billing");
-        // Delivery Debugging Purposes
-        System.out.println("Delivery Address");
-        System.out.println(bca.getAddress());
-        System.out.println(bca.getCity());
-        System.out.println(bca.getZipcode());
-        System.out.println(bca.getRegion());
-        System.out.println(bca.getCountry());
-        System.out.println(bca.getAddressType());
-        System.out.println(bca.getAddressType());
-        // Shipping Address
-        dca.setAddress(request.getParameter("d_address"));
-        dca.setCity(request.getParameter("d_city"));
-        dca.setZipcode(request.getParameter("d_zipcode"));
-        dca.setRegion(request.getParameter("d_region"));
-        dca.setCountry(request.getParameter("d_country"));
-        dca.setAddressType("delivery");
-        // Delivery Debugging Purposes
-        System.out.println("Delivery Address");
-        System.out.println(dca.getAddress());
-        System.out.println(dca.getCity());
-        System.out.println(dca.getZipcode());
-        System.out.println(dca.getRegion());
-        System.out.println(dca.getCountry());
-        System.out.println(dca.getAddressType());
-        
-        // Set user credit card info
-        cc.setName(request.getParameter("cardName"));
-        cc.setCardNum(BCrypt.hashpw(request.getParameter("cardNum"), BCrypt.gensalt(10)));
-        cc.setType(request.getParameter("cardType"));
-        cc.setExpDate(request.getParameter("cardExp"));
-        System.out.println("YUNG CARD TYPE: " + request.getParameter("cardType"));
-        
-        try {
             isSuccessful = AccountService.registerUser(nc,bca,dca,cc);
-            System.out.println("SUCCESSFUL BA BES?" + isSuccessful);
+
             if (isSuccessful) {
                 // Set password
                 int userID;
@@ -162,15 +162,23 @@ public class RegisterServlet extends HttpServlet {
                 if(userID > 0) {
                     ps.setUserID(userID);
                     SecurityService.addPassword(ps);
+                    
+                    responseCode = Audit.OKINFO;
+                    msgDesc = "A New User Registered";
+                    
                     request.getRequestDispatcher("main-login-page.jsp").forward(request, response);
                 }
             }
-            
-            // add else if registration is not successful
+            else {
+                // error logger here
+            }
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
+            responseCode = Audit.CLASSNOTFOUNDEX;
+            response.sendError(responseCode, Audit.getHttpStatusMsg(responseCode));
         }
-        
+        finally {
+            Audit.getAuditLog(userID, responseCode, msgDesc);
+        }
     }
 
     /**
