175bc1f2983cc055ad15cffd3b40950703b5a507
Allendale Nato <natoallendale@gmail.com>
2017-03-26 20:00:12 +0800

    Add noty in dependencies

--- pages/js/product-item.js
+++ pages/js/product-item.js
@@ -14,9 +14,12 @@ $('#postActionBTN').click(function(event) {
   })
   .done(function() {
     console.log("success");
+    generateAlert('success', '<h4 style="color:white">Your review has been posted successfully :-)</h4>')
   })
   .fail(function() {
     console.log("error");
+    generateAlert('error',
+    '<h4 style="color:white">Oops, Something went wrong posting your review </br></h4> Try Again Later :(');
   })
   .always(function() {
     console.log("complete");
@@ -29,3 +32,23 @@ $('#postActionBTN').click(function(event) {
 $('#reviewModal').on('hidden.bs.modal', function () {
   $(this).find("input,textarea,select").val('').end();
 });
+
+function generateAlert(type, message){
+  var n = noty({
+      text        : message,
+      type        : type,
+      dismissQueue: true,
+      progressBar : true,
+      timeout     : 5000,
+      layout      : 'topRight',
+      closeWith   : ['click'],
+      theme       : 'relax',
+      maxVisible  : 10,
+      animation   : {
+          open  : 'animated bounceInRight',
+          close : 'animated bounceOutRight',
+          easing: 'swing',
+          speed : 500
+      }
+  });
+}
