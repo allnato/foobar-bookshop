e01aa477c41680238935f30be2b719365ee5fc39
Mark Sanchez <dlsu.sachii@gmail.com>
2017-03-28 04:49:01 +0800

    Something

--- FooBarBookshop/build/web/js/my-cart.js
+++ FooBarBookshop/build/web/js/my-cart.js
@@ -23,6 +23,7 @@ $('.removeRowBTN').click(function(event) {
   $('.total-modal').text(removeRow[0].cells[3].innerText);
   // Open a Modal
   $('#removeItemModal').modal('show');
+  $('#removeActionBTN').unbind('click');
   $('#removeActionBTN').click(function(event) {
     // Confirm: AJAX
     $.ajax({
@@ -38,6 +39,7 @@ $('.removeRowBTN').click(function(event) {
     })
     .done(function() {
       console.log("success");
+      generateAlert('success', '<h4 style="color: white">Your item is successfully removed.</h4>');
       // Remove Row
       removeRow.remove();
       // Update Total Price
@@ -45,6 +47,7 @@ $('.removeRowBTN').click(function(event) {
       $('.total-amount').text(totalPrice.toFixed(2) + " PHP")
     })
     .fail(function() {
+      generateAlert('error', '<h4 style="color: white">Oops, Something went wrong removing your item</h4> </br> Try again later.');
       console.log("error");
     })
     .always(function() {
@@ -54,3 +57,23 @@ $('.removeRowBTN').click(function(event) {
     $('#removeItemModal').modal('hide');
   });
 });
+
+function generateAlert(type, message){
+  var n = noty({
+      text        : message,
+      type        : type,
+      dismissQueue: true,
+      progressBar : true,
+      timeout     : 5000,
+      layout      : 'topRight',
+      closeWith   : ['click'],
+      theme       : 'relax',
+      maxVisible  : 10,
+      animation   : {
+          open  : 'animated bounceInRight',
+          close : 'animated bounceOutRight',
+          easing: 'swing',
+          speed : 500
+      }
+  });
+}
